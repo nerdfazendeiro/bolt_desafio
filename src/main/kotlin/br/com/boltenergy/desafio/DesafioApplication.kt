089291edaf4d2830e949a55225b01d49ecb74a85
Rafael Ramalho Silveira <rafael@techrafael.com.br>
2025-04-07 21:08:02 -0400

    Initial commit with Spring Boot project structure

---
+++ src/main/kotlin/br/com/boltenergy/desafio/DesafioApplication.kt
@@ -0,0 +1,11 @@
+package br.com.boltenergy.desafio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesafioApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesafioApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/boltenergy/desafio/DesafioApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.boltenergy.desafio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
